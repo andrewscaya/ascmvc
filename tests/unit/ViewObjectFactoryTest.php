9952a07470895966ac5c672d6131a11f746598ed
Andrew Caya <andrewscaya@yahoo.ca>
2019-01-21 21:31:34 -0500

    2019-01-21 AC: Updates the version number.

--- tests/unit/ViewObjectFactoryTest.php
+++ tests/unit/ViewObjectFactoryTest.php
@@ -1,29 +1,13 @@
 <?php
-
 /**
  * LightMVC/ASCMVC
  *
- * Copyright 2010 - 2019 Foreach Code Factory <ascmvc@asclinux.net>
- * Version 2.0.1
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @package    lightmvc/ascmvc
- * @copyright  Copyright 2010 - 2019 Foreach Code Factory <ascmvc@asclinux.net>
- * @link       http://lightmvc.net/
- * @license    Apache License, Version 2.0, see above
- * @license    http://www.apache.org/licenses/LICENSE-2.0
- * @since 2.0.0
+ * @package    LightMVC/ASCMVC
+ * @author     Andrew Caya
+ * @link       https://github.com/lightmvc/ascmvc
+ * @version    2.0.2
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0.
+ * @since      2.0.0
  */
 
 namespace AscmvcTest;
