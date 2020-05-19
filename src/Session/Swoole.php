43417a16bbafe80c8b8c3bb5824bee9dffa55cf4
Andrew Caya <andrewscaya@yahoo.ca>
2020-05-19 16:30:21 -0400

    2020-05-19 AC: Updates the version number.

--- src/Session/Swoole.php
+++ src/Session/Swoole.php
@@ -5,7 +5,7 @@
  * @package    LightMVC/ASCMVC
  * @author     Andrew Caya
  * @link       https://github.com/lightmvc/ascmvc
- * @version    3.3.0
+ * @version    4.0.0
  * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0.
  * @since      2.1.0
  *
