989680ab3e2b0dc731b3940c011efc85db10fb56
Andrew Caya <andrewscaya@yahoo.ca>
2023-01-10 12:48:48 -0500

    2023-01-10 AC: Updates the main CommandRunner class (Symfony Process).

--- src/EventSourcing/CommandRunner.php
+++ src/EventSourcing/CommandRunner.php
@@ -130,32 +130,23 @@ class CommandRunner
 
                 $arguments = '';
 
+                $commandline[] = 'php';
+                $commandline[] = $asyncBus;
+                $commandline[] = $this->name;
+
                 if (isset($this->arguments['execute'])) {
                     $execute = $this->arguments['execute'];
 
                     unset($this->arguments['execute']);
 
-                    $arguments .= $execute . ' ';
+                    $commandline[] = $execute;
                 }
 
                 if (isset($this->arguments['--values'])) {
                     $values = $this->arguments['--values'];
 
-                    $arguments .= '--values ' . escapeshellarg($values) . ' ';
-                }
-
-                if (!empty($arguments)) {
-                    $commandline = 'php '
-                        . $asyncBus
-                        . ' '
-                        . $this->name
-                        . ' '
-                        . $arguments;
-                } else {
-                    $commandline = 'php '
-                        . $asyncBus
-                        . ' '
-                        . $this->name;
+                    $commandline[] = '--values';
+                    $commandline[] = $values;
                 }
 
                 $this->commandProcess = new Process($commandline);
@@ -169,7 +160,7 @@ class CommandRunner
                     $this->commandProcess->mustRun();
                 } catch (ProcessFailedException $e) {
                     $this->error = $e->getMessage();
-                    throw new \Exception($commandline . ' failed');
+                    throw new \Exception(implode(' ', $commandline) . ' failed');
                 }
                 // @codeCoverageIgnoreEnd
 
